cdefc5f17e88645be04bf0e8695127ac21c6e7cf
David <davidlarson46@yahoo.com>
2019-05-20 08:31:49 -0600

    imported random, enum method for shapes, set coordinates for shapes.

--- src/gameshapes/gameShapes.java
+++ src/gameshapes/gameShapes.java
@@ -1,6 +1,40 @@
 package gameshapes;
 
+import java.util.Random;
+
 public class gameShapes
 {
+	 protected enum Tetrominoe 
+	 { 
+		 NoShape, ZShape, SShape, LineShape, 
+         TShape, SquareShape, LShape, MirroredLShape 
+     };
+
+private Tetrominoe pieceShape;
+private int coords[][];
+private int[][][] coordsTable;
 
 }
+
+public Shape() 
+{
+
+    coords = new int[4][2];
+    setShape(Tetrominoe.NoShape);
+}
+
+public void setShape(Tetrominoe shape) {
+
+     coordsTable = new int[][][] {
+        { { 0, 0 },   { 0, 0 },   { 0, 0 },   { 0, 0 } },
+        { { 0, -1 },  { 0, 0 },   { -1, 0 },  { -1, 1 } },
+        { { 0, -1 },  { 0, 0 },   { 1, 0 },   { 1, 1 } },
+        { { 0, -1 },  { 0, 0 },   { 0, 1 },   { 0, 2 } },
+        { { -1, 0 },  { 0, 0 },   { 1, 0 },   { 0, 1 } },
+        { { 0, 0 },   { 1, 0 },   { 0, 1 },   { 1, 1 } },
+        { { -1, -1 }, { 0, -1 },  { 0, 0 },   { 0, 1 } },
+        { { 1, -1 },  { 0, -1 },  { 0, 0 },   { 0, 1 } }
+    };
+    
+    
+
